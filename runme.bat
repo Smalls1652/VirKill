c235d54e9ce7f64c04ee48372f155cfb72d527af
Smalls <Smalls1652@gmail.com>
2016-02-04 00:40:02 -0500

    Base upload

---
+++ runme.bat
@@ -0,0 +1,21 @@
+@setlocal enableextensions
+@cd /d "%~dp0"
+@echo off
+
+title VirKill Downloader
+
+Ping www.google.com -n 1 >NUL
+if errorlevel 1 goto :copyfiles
+
+powershell.exe -ExecutionPolicy Bypass -File virkill.ps1
+
+:copyfiles
+mkdir %USERPROFILE%\Desktop\VirusTools >NUL
+
+xcopy /S /Y .\files %USERPROFILE%\Desktop\VirusTools
+
+echo.
+
+echo VirKill downloader completed.
+
+pause
