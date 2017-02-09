65fcbbaa4b173f479699f34ee0649419c17a1c42
Smalls <Smalls1652@gmail.com>
2017-02-09 11:16:57 -0500

    Made a download database
    The script now reads from a CSV database file that carries the data. This is more robust than doing a while statement and having 6 different arrays.

--- virkill.ps1
+++ virkill.ps1
@@ -1,3 +1,4 @@
+[CmdletBinding()]
 param(
 	[bool]$force	
 	)
@@ -36,72 +37,22 @@ if($error) {
 cls
 Write-Warning "Something went wrong with the download."
 Write-Warning "Copying current local files."
-exit
-} 
- 
-#Begin BleepingComputer list.
-$downloadlinks = @(
-                        "http://www.bleepingcomputer.com/download/combofix/dl/12/",
-                        "http://www.bleepingcomputer.com/download/adwcleaner/dl/125/",
-                        "http://www.bleepingcomputer.com/download/rkill/dl/10/",
-                        "http://www.bleepingcomputer.com/download/minitoolbox/dl/65/",
-						"http://www.bleepingcomputer.com/download/junkware-removal-tool/dl/293/",
-						"http://www.bleepingcomputer.com/download/hijackthis/dl/89/"
-					)
-                                       
-$downloadparse = @(
-                        'https://download.bleepingcomputer.com/dl/+[\w-]+(/[\w- ./?%&=]*)*/ComboFix.exe',
-                        'https://download.bleepingcomputer.com/dl/+[\w-]+(/[\w- ./?%&=]*)*/AdwCleaner.exe',
-                        'https://download.bleepingcomputer.com/dl/+[\w-]+(/[\w- ./?%&=]*)*/rkill.exe',
-                        'https://download.bleepingcomputer.com/dl/+[\w-]+(/[\w- ./?%&=]*)*/MiniToolBox.exe',
-                        'https://download.bleepingcomputer.com/dl/+[\w-]+(/[\w- ./?%&=]*)*/JRT.exe',
-						'https://download.bleepingcomputer.com/dl/+[\w-]+(/[\w- ./?%&=]*)*/HijackThis.exe'
-					)
- 
-$downloadname = @(
-                        "ComboFix",
-                        "ADWCleaner",
-                        "rkill",
-                        "MiniToolBox",
-						"JRT",
-						"HijackThis"
-					)
- 
-$dlinksarraylength = $downloadlinks.length
-$dlinksarraylength--
-#End BleepingComputer list.
+}
+}
 
-#Begin Custom Download list.
-#Only include static links.
-$customdownloadlinks = @(
-							'http://dl.emsisoft.com/EmsisoftEmergencyKit.exe',
-							'https://downloads.malwarebytes.org/file/mbam_current/',
-							'http://data.mbamupdates.com/tools/mbam-rules.exe'
-						)
-$customdownloadname = @(
-							'EmsisoftEmergencyKit',
-							'Malwarebytes',
-							'Malwarebytes Defintions'
-						)
-
-$customdownloadexe = @(
-							'EmsisoftEmergencyKit',
-							'mbam-setup',
-							'mbam-rules'
-						)
-$customdownloadlength = $customdownloadlinks.length
-$customdownloadlength--
-#End Custom Download list.
-
-$i = 0
-$s = 0
+function runDownload
+{
  
-while ($i -le $dlinksarraylength)
-		{
-                $dlink = $downloadlinks[$i]
-                $dname = $downloadname[$i]
-                $dparse = $downloadparse[$i]
-				
+$dloader = New-Object System.Net.WebClient
+
+$downloadCSV = Import-Csv .\downloadlinks.csv
+ 
+ForEach ($bleepDload in ( $downloadCSV | Where-Object -Property Type -EQ "Bleeping" ))
+				{
+
+                $dname = $bleepDload.Name
+                $dlink = $bleepDload.DownloadLink
+                $dparse = $bleepDload.DownloadParse
 				$host.ui.RawUI.WindowTitle = "VirKill (Downloading $dname)"
 
 				Write-host
@@ -119,29 +70,29 @@ while ($i -le $dlinksarraylength)
 					Write-host "Recently updated. ($dhours hour(s) ago)"
 					Write-host "Skipping."
 					Write-host
-					$i++
 					continue
 					}
 				}
                 }
 				$htmlfile = "$storageDir\Download.html"
-                $webclient.DownloadFile($dlink,$htmlfile)
+                $dloader.DownloadFile($dlink,$htmlfile)
+
+                Write-Debug $htmlfile
 				
 				$purl = select-string -Path $storageDir\Download.html -Pattern $dparse | % { $_.Matches} | % { $_.Value} | Select -Skip 1
 			
 				
                 $dfilename = "$storageDir\files\$dname.exe"
-                $webclient.DownloadFile($purl,$dfilename)
+                $dloader.DownloadFile($purl,$dfilename)
                 Remove-Item "$storageDir\Download.html"
 				
-                $i++
         }
 
-while ($s -le $customdownloadlength)
+ForEach ($custDload in ( $downloadCSV | Where-Object -Property Type -EQ "Custom" ))
 		{
-                $dlink = $customdownloadlinks[$s]
-                $dname = $customdownloadname[$s]
-                $dexe = $customdownloadexe[$s]
+                $dlink = $custDload.DownloadLink
+                $dname = $custDload.Name
+                $dexe = $custDload.EXEName
 				
 				$host.ui.RawUI.WindowTitle = "VirKill (Downloading $dname)"
 				
@@ -160,15 +111,12 @@ while ($s -le $customdownloadlength)
 					Write-host "Recently updated. ($dhours hour(s) ago)"
 					Write-host "Skipping."
 					Write-host
-					$s++
 					continue
 					}
 				}
 				}
                 $dfilename = "$storageDir\files\$dexe.exe"
-                $webclient.DownloadFile($dlink,$dfilename)
-               
-                $s++
+                $dloader.DownloadFile($dlink,$dfilename)
         }
 }
 
@@ -181,18 +129,20 @@ do
         '1'{
             cls
             mainRun
-            New-Item -type directory C:\Users\$env:UserName\Desktop\VirKill\
-            Copy-Item $storageDir\files\* C:\Users\$env:UserName\Desktop\VirKill\
+            runDownload
+            New-Item -type directory C:\Users\$env:UserName\Desktop\VirusTools\
+            Copy-Item $storageDir\files\* C:\Users\$env:UserName\Desktop\VirusTools\
             }
         '2'{
             cls
             $force = $true
             mainRun
+            runDownload
             }
         '3'{
             cls
-            New-Item -type directory C:\Users\$env:UserName\Desktop\VirKill\
-            Copy-Item $storageDir\files\* C:\Users\$env:UserName\Desktop\VirKill\
+            New-Item -type directory C:\Users\$env:UserName\Desktop\VirusTools\
+            Copy-Item $storageDir\files\* C:\Users\$env:UserName\Desktop\VirusTools\
             }            
         '0'{
             return
